815f4f408694f6a18b6bea8aa9ed2118b85a8555
Sara <sara.mulas89@gmail.com>
2019-11-19 20:22:57 +0100

    changes homepage

--- LoginJFX/src/application/Controlador.java
+++ LoginJFX/src/application/Controlador.java
@@ -1,9 +1,16 @@
 package application;
 
 import javafx.util.Duration;
+
+import java.io.IOException;
+
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class Controlador {
 
@@ -18,6 +25,9 @@ public class Controlador {
 
 	@FXML
 	private Pane pane4;
+	
+	@FXML
+	AnchorPane rootPane;
 
 	public void initialize() {
 
@@ -29,7 +39,33 @@ public class Controlador {
 		backgroundAnimation();
 
 	}
+	
+	@FXML
+	public void abrirHomepage() {
+		
+		cerrarVentana();
+		
+		try {
+			
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Homepage.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            Stage sendStage = new Stage();    
+            Scene scene = new Scene(page);
+            sendStage.setScene(scene);          
+            sendStage.show();
+		
 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	private void cerrarVentana() {
+		Stage thisStage = (Stage) rootPane.getScene().getWindow();
+	    thisStage.close();	
+	}
+	
 	private void backgroundAnimation() {
 
 		FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), pane4);
